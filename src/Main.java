4fc6a010e8060ca24fdc07b8c1202d16354d6d45
zhafa anbiya ananta farrel <zhafaanbiya311@gmail.com>
2022-10-25 09:12:31 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+       for (int x=1 ; x <= 50 ; x++){
+           System.out.println("BAKSO KONTOL");
+
+       }
+    }
+}
